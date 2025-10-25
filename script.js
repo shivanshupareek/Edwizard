08415ccd6558e0b7191d90004802edb8b55998db
shivanshupareek <shivanshupareek.ps@gmail.com>
2025-10-25 18:03:27 +0530

    restoring lost data

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-const hamburger = document.getElementById('hamburger');
-const hamburgerIcon = document.getElementById('hamburger-icon');
-const mobileMenu = document.getElementById('mobile-menu');
-const dropdown = document.querySelector('.mobile-link.dropdown')
+const hamburger = document.getElementById("hamburger");
+const hamburgerIcon = document.getElementById("hamburger-icon");
+const mobileMenu = document.getElementById("mobile-menu");
+const dropdown = document.querySelector(".mobile-menu .mobile-link.dropdown");
 
 hamburger.addEventListener("click", () => {
-    mobileMenu.classList.toggle('show');
-    const isOpen = mobileMenu.classList.contains('show');
-    hamburgerIcon.src = isOpen ? "assets/cross-icon.png" : "assets/hamburger-icon.png";
+  mobileMenu.classList.toggle("show");
+  const isOpen = mobileMenu.classList.contains("show");
+  hamburgerIcon.src = isOpen
+    ? "assets/cross-icon.png"
+    : "assets/hamburger-icon.png";
 });
 
-dropdown.addEventListener("click", (e) => {
+if (dropdown) {
+  dropdown.addEventListener("click", (e) => {
     e.stopPropagation();
-    dropdown.classList.toggle('show');
-});
+    dropdown.classList.toggle("show");
+  });
+}
